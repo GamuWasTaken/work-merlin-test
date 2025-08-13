df2d7ee6d2ef82ef51bbd8ad0536bd85f3c43d85
GamuWasTaken <victordiezruiz@proton.me>
2025-08-13 11:45:29 +0200

    Added 2 more tests for: bad weights and unpaired lists

--- src/test/java/es/merlinsoftware/SortingScoresTest.java
+++ src/test/java/es/merlinsoftware/SortingScoresTest.java
@@ -33,4 +33,44 @@ public class SortingScoresTest {
         Assert.assertArrayEquals(Solution.sortProductsByScores(50, 50, productStock, productSales).toArray(),
                 expectedResult);
     }
+    @Test(expected = IllegalArgumentException.class)
+    public void testBadWeights() {
+        List<ProductSales> productSales = new ArrayList<>();
+        productSales.add(new ProductSales(1L, 10000.0));
+        productSales.add(new ProductSales(2L, 50000.0));
+        productSales.add(new ProductSales(3L, 100000.0));
+        productSales.add(new ProductSales(4L, 75000.0));
+
+
+        List<ProductStock> productStock = new ArrayList<>();
+        productStock.add(new ProductStock(1L, 100000L));
+        productStock.add(new ProductStock(2L, 400000L));
+        productStock.add(new ProductStock(3L, 200000L));
+        productStock.add(new ProductStock(4L, 300000L));
+
+
+        Long[] expectedResult = {2L, 4L, 3L, 1L};
+
+        Assert.assertArrayEquals(Solution.sortProductsByScores(0, 50, productStock, productSales).toArray(),
+                expectedResult);
+    }
+    @Test
+    public void testUnpairedLists() {
+        List<ProductSales> productSales = new ArrayList<>();
+        productSales.add(new ProductSales(1L, 10000.0));
+        productSales.add(new ProductSales(4L, 75000.0));
+
+
+        List<ProductStock> productStock = new ArrayList<>();
+        productStock.add(new ProductStock(2L, 400000L));
+        productStock.add(new ProductStock(1L, 100000L));
+        productStock.add(new ProductStock(3L, 200000L));
+        productStock.add(new ProductStock(4L, 300000L));
+
+
+        Long[] expectedResult = {2L, 4L, 3L, 1L};
+
+        Assert.assertArrayEquals(Solution.sortProductsByScores(50, 50, productStock, productSales).toArray(),
+                expectedResult);
+    }
 }
