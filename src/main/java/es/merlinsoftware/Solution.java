89532e7ac1bcb7d7661ece2329b063d6c1e46296
GamuWasTaken <victordiezruiz@proton.me>
2025-08-13 11:44:41 +0200

    Implemented solution

--- src/main/java/es/merlinsoftware/Solution.java
+++ src/main/java/es/merlinsoftware/Solution.java
@@ -4,16 +4,44 @@ import es.merlinsoftware.pojo.ProductSales;
 import es.merlinsoftware.pojo.ProductStock;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution {
 
     public static List<Long> sortProductsByScores(int stockWeight, int salesWeight,
                                                   List<ProductStock> productsStockInformation,
                                                   List<ProductSales> productsSalesInformation) {
+        if (stockWeight + salesWeight != 100) {
+            throw new IllegalArgumentException("Weights must add to 100");
+        }
 
-        //TODO: Complete this method with the solution, you don't need to split your solution over multiple classes,
-        // just get it done!
-        return new ArrayList<>();
+        Map<Long, Double> salesMap = new HashMap<>();
+
+        productsSalesInformation
+            .stream()
+            .forEach(sale -> salesMap.put(sale.getProductId(), sale.getSalesAmount()));
+
+        Map<Long, Double> scores = new HashMap<>();
+
+        productsStockInformation.stream().forEach(stock -> {
+            double saleAmount = salesMap.getOrDefault(stock.getProductId(), 0.0);
+            double score = (stockWeight / 100.0 * stock.getAvailableStock()) + (salesWeight / 100.0 * saleAmount);
+
+            scores.put(stock.getProductId(), score);
+        });
+
+        List<Long> result = scores
+            .entrySet()
+            .stream()
+            .sorted(
+                (s1, s2) -> 
+                    Double.compare(s2.getValue(), s1.getValue())
+            )
+            .map(entry -> entry.getKey())
+            .collect(Collectors.toList())
+        ;
+
+        return result;
     }
 
 }
